cc0a345f668f4c083ccc1ecbb2e0211fb2a10abd
fufeng1226 <15281205719@163.com>
2020-03-17 13:37:00 +0800

    map size calculate

--- src/main/java/com/lcydream/project/jguid/datastructure/map/MapTest.java
+++ src/main/java/com/lcydream/project/jguid/datastructure/map/MapTest.java
@@ -27,6 +27,35 @@ public class MapTest {
         System.out.println("oldest entry should be removed");
         accessOrderedMap.forEach((k,v)-> System.out.println(k+":"+v));
 
+        System.out.println(tableSizeFor(3));
+        System.out.println(tableSize(6));
+    }
+
+    static final int MAXIMUM_CAPACITY = 1073741824;
+
+    /**
+     * jdk 11 for HashMap
+     * @param cap 容量
+     * @return
+     */
+    static final int tableSizeFor(int cap) {
+        int n = -1 >>> Integer.numberOfLeadingZeros(cap - 1);
+        return n < 0 ? 1 : (n >= 1073741824 ? 1073741824 : n + 1);
+    }
+
+    /**
+     * jdk 8 for HashMap
+     * @param cap 容量
+     * @return
+     */
+    static final int tableSize(int cap) {
+        int n = cap - 1;
+        n |= n >>> 1;
+        n |= n >>> 2;
+        n |= n >>> 4;
+        n |= n >>> 8;
+        n |= n >>> 16;
+        return (n < 0) ? 1 : (n >= MAXIMUM_CAPACITY) ? MAXIMUM_CAPACITY : n + 1;
     }
 
 }
