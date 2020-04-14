320b9fba0b1e63649c73ad49b081ab4629c0bbb2
MagicLuo <15281205719@163.com>
2020-04-14 18:02:14 +0800

    线程相关

--- src/main/java/com/lcydream/project/jguid/mxbean/GetThread.java
+++ src/main/java/com/lcydream/project/jguid/mxbean/GetThread.java
@@ -15,14 +15,20 @@ import java.lang.management.ThreadMXBean;
 public class GetThread {
 
     public static void main(String[] args) {
+        // 获取 Java 线程管理 MXBean
         ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
+        //获取所有线程ID
         long[] threadIds = threadMXBean.getAllThreadIds();
         ThreadInfo[] threadInfos = threadMXBean.getThreadInfo(threadIds);
         for (ThreadInfo threadInfo : threadInfos) {
             System.out.println(threadInfo.getThreadId()+": "+threadInfo.getThreadName());
         }
-        while (true){
-
+        System.out.println("-----------------");
+        // 不需要获取同步的 monitor 和 synchronizer 信息，仅获取线程和线程堆栈信息
+        ThreadInfo[] threadsInfo = threadMXBean.dumpAllThreads(false, false);
+        // 遍历线程信息，仅打印线程 ID 和线程名称信息
+        for (ThreadInfo threadInfo : threadsInfo) {
+            System.out.println("[" + threadInfo.getThreadId() + "] " + threadInfo.getThreadName());
         }
     }
 }
