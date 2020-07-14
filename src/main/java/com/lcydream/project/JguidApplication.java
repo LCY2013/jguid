16cdc582e6ed3c0ac65984221a953e392f88da19
MagicLuo <15281205719@163.com>
2020-07-14 10:04:36 +0800

    加入函数编程依赖

--- src/main/java/com/lcydream/project/JguidApplication.java
+++ src/main/java/com/lcydream/project/JguidApplication.java
@@ -2,12 +2,21 @@ package com.lcydream.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JguidApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JguidApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JguidApplication.class, args);
+    }
+
+    @GetMapping("/get/{username}")
+    public String getName(@PathVariable String username) {
+        return username + ":" + System.currentTimeMillis();
+    }
 
 }
