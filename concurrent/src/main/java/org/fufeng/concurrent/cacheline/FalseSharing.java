ce1dbe75d53d5c0de62a0375323909b8d96c9ba6
MagicLuo <15281205719@163.com>
2020-10-29 18:05:58 +0800

    CPU 缓存行测试用例

---
+++ concurrent/src/main/java/org/fufeng/concurrent/cacheline/FalseSharing.java
@@ -0,0 +1,79 @@
+/*
+ * The MIT License (MIT)
+ * ------------------------------------------------------------------
+ * Copyright © 2019 Ramostear.All Rights Reserved.
+ *
+ * ProjectName: jguid
+ * @Author : <a href="https://github.com/lcy2013">MagicLuo(扶风)</a>
+ * @date : 2020-10-29
+ * @version : 1.0.0-RELEASE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+package org.fufeng.concurrent.cacheline;
+
+/**
+ * @author <a href="https://github.com/lcy2013">MagicLuo(扶风)</a>
+ * @program jguid
+ * @description 不使用缓存行基准测试
+ * @create 2020-10-29
+ */
+public final class FalseSharing implements Runnable {
+    public final static int NUM_THREADS = 4; // change
+    public final static long ITERATIONS = 500L * 1000L * 1000L;
+    private final int arrayIndex;
+
+    private static final VolatileLong[] longs = new VolatileLong[NUM_THREADS];
+
+    static {
+        for (int i = 0; i < longs.length; i++) {
+            longs[i] = new VolatileLong();
+        }
+    }
+
+    public FalseSharing(final int arrayIndex) {
+        this.arrayIndex = arrayIndex;
+    }
+
+    public static void main(final String[] args) throws Exception {
+        final long start = System.nanoTime();
+        runTest();
+        System.out.println("duration = " + (System.nanoTime() - start));
+    }
+
+    private static void runTest() throws InterruptedException {
+        Thread[] threads = new Thread[NUM_THREADS];
+
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(new FalseSharing(i));
+        }
+
+        for (Thread t : threads) {
+            t.start();
+        }
+
+        for (Thread t : threads) {
+            t.join();
+        }
+    }
+
+    public void run() {
+        long i = ITERATIONS + 1;
+        while (0 != --i) {
+            longs[arrayIndex].value = i;
+        }
+    }
+
+    public final static class VolatileLong {
+        public volatile long value = 0L;
+        public long p1, p2, p3, p4, p5, p6; // comment out
+
+        // 注释掉上面的对象填充用时: 32949879471
+        // 加上上面的对象填充用时:   16253414739
+    }
+}
