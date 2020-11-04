2ff9c606059740c41fa7c38c22201ae64090ddd8
fufeng <15281205719@163.com>
2020-11-04 18:04:01 +0800

    直接内存使用 jvm相关启动参数调整

--- jvm/src/main/java/org/fufeng/project/jvm/outofmemory/DirectMemoryOOM.java
+++ jvm/src/main/java/org/fufeng/project/jvm/outofmemory/DirectMemoryOOM.java
@@ -6,8 +6,10 @@ import java.lang.reflect.Field;
 
 /**
  * DirectMemoryOOM
- * VM Args: -X:Xms20M -XX:MaxDirectMemorySize=10M
+ * VM Args: -Xms20M -XX:MaxDirectMemorySize=10M
  * 设置最大直接内存
+ *
+ * <a href="https://docs.oracle.com/javase/8/docs/technotes/tools/unix/java.html#BGBCIEFC">java 参数信息<a/>
  * @author Luo Chun Yun
  * @email 1475653689@qq.com
  * @date 2019/2/27 11:55
@@ -17,9 +19,9 @@ public class DirectMemoryOOM {
 	private static final int _1M = 1024*1024;
 
 	public static void main(String[] args) throws IllegalAccessException {
-		Field unsageField = Unsafe.class.getDeclaredFields()[0];
-		unsageField.setAccessible(true);
-		Unsafe unsafe = (Unsafe) unsageField.get(null);
+		Field unsafeField = Unsafe.class.getDeclaredFields()[0];
+		unsafeField.setAccessible(true);
+		Unsafe unsafe = (Unsafe) unsafeField.get(null);
 		while (true){
 			unsafe.allocateMemory(_1M);
 		}
