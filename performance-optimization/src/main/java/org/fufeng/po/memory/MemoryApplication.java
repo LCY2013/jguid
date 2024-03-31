79f087b7bd3c65d2e99e2dbc296e39f45fb6dd73
fufeng <15281205719@163.com>
2024-03-31 21:45:08 +0800

    Feat: performance optimization jvm heap/nonheap oom with mat/jmap

--- performance-optimization/src/main/java/org/fufeng/po/memory/MemoryApplication.java
+++ performance-optimization/src/main/java/org/fufeng/po/memory/MemoryApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * memory
  * 1、内存溢出自动导出
- *
+ *  -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./heapdump.hprof
+ * 2、使用jmap命令手动导出
+ *  jmap -dump:format=b,file=heapdump.hprof PID
  *
  * @author fufeng
  * @Date 2024-03-31 20:59
