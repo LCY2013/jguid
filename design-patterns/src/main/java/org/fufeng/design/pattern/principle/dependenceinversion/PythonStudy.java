d44027e4bff95e3092939c3d05643f453b388a9c
fufeng <15281205719@163.com>
2024-04-04 16:22:08 +0800

    Feat: dependence inversion principle

---
+++ design-patterns/src/main/java/org/fufeng/design/pattern/principle/dependenceinversion/PythonStudy.java
@@ -0,0 +1,16 @@
+package org.fufeng.design.pattern.principle.dependenceinversion;
+
+/**
+ * TODO
+ *
+ * @author fufeng
+ * {@code @Date} 2024-04-04 16:02
+ */
+public class PythonStudy implements IStudy {
+
+    @Override
+    public void study() {
+        System.out.println("user study python course");
+    }
+
+}
